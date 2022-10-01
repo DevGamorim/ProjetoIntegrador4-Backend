118e1b8a31493533d6f3220b347d05c1b138169c
DevGamorim <78799375+DevGamorim@users.noreply.github.com>
2022-10-01 10:25:19 -0300

    create methods and edit models

--- Backend/Data/models.py
+++ Backend/Data/models.py
@@ -6,32 +6,55 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class Users(AbstractUser):
+    UserStats = (
+        (True,'Ativo'),
+        (False,'Inativo')
+    )
+
     UserFirstName = models.CharField(max_length=255)
     UserLastName = models.CharField(max_length=255)
     UserCPF = models.CharField(max_length=12)
     UserEmail = models.CharField(max_length=255)
+    UserStats = models.BooleanField(choices=UserStats)
+
+class Groups(models.Model):
+    GpStats = (
+        (True,'Ativo'),
+        (False,'Inativo')
+    )
+
+    GpId = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    GpUse = models.ForeignKey(Users, on_delete=models.CASCADE, related_name="GrupoUsers")
+    GpName = models.CharField(max_length=255)
+    GpStats = models.BooleanField(choices=GpStats)
 
 class Financas(models.Model):
     FinancaType = (
         (True,'Entrada'),
         (False,'Saida')
     )
+    FinancaStats = (
+        (True,'Ativo'),
+        (False,'Inativo')
+    )
 
     FinancaID = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    FinancaUser = models.ForeignKey(Users, on_delete=models.CASCADE)
+    FinancaGroup = models.ForeignKey(Groups, on_delete=models.CASCADE)
     FinancaType = models.BooleanField(choices=FinancaType)
     FinancaValue = models.FloatField()
     FinancaDateCreate = models.DateTimeField(auto_now_add=True,unique=False, null=True)
     FinancaDateUpdate = models.DateTimeField(auto_now=True,unique=False, null=True)
-    
-class Groups(models.Model):
-    GroupID = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    GroupName = models.CharField(max_length=255)
+    FinancaStats = models.BooleanField(choices=FinancaStats)
 
 class Caixas(models.Model):
+    CaixaStats = (
+        (True,'Ativo'),
+        (False,'Inativo')
+    )
+
     CaixaID = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    CaixaGroup = models.ForeignKey(Groups, on_delete=models.CASCADE)
     CaixaFinanca = models.ForeignKey(Financas, on_delete=models.CASCADE)
     CaixaTotal = models.FloatField()
+    CaixaStats = models.BooleanField(choices=CaixaStats)
 
 #HOgLeGFqrwpZ1rQE
--- Backend/Endpoint/views.py
+++ Backend/Endpoint/views.py
@@ -7,6 +7,38 @@ from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 # Create your views here.
 
+from Data.models import Users, Financas, Groups, Caixas
+
 #@method_decorator(login_required, name='dispatch')
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+def create_group(request):
+    user = request.user
+    novo_grupo = Groups.objects.create()
+    return 0
+
+def edit_group(request):
+    user = request.user
+    edit = Groups.objects.filter(GpUse=user).update(field1='some value')
+    return 0
+
+def create_financas(request):
+    user = request.user
+    novo_grupo = Financas.objects.create()
+    return 0
+
+def edit_financas(request):
+    user = request.user
+    edit = Financas.objects.filter(GpUse=user).update(field1='some value')
+    return 0
+
+def create_caixas(request):
+    user = request.user
+    novo_grupo = Caixas.objects.create()
+    return 0
+
+def edit_caixa(request):
+    user = request.user
+    edit = Caixas.objects.filter(GpUse=user).update(field1='some value')
+    return 0
