b2d15f1244e8480bff69853cf4a78d5e63a31c60
DevGamorim <78799375+DevGamorim@users.noreply.github.com>
2022-10-01 09:22:51 -0300

    create init home

--- Backend/Endpoint/views.py
+++ Backend/Endpoint/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import TemplateView
+# Create your views here.
+
+#@method_decorator(login_required, name='dispatch')
+class HomePageView(TemplateView):
+    template_name = 'home.html'
